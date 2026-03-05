075628c424f022a88f2455dd4f999bbf0c28f56a
Umer Nawaz Khan <omer034000@gmail.com>
2026-03-05 22:46:05 +0500

    logical operators in Python

--- logical_operators.py
+++ logical_operators.py
@@ -1,17 +1,18 @@
-# Logical Operators (AND, OR, NOT)
+# Logical Operators in Python: AND, OR, NOT
 
-# NOT
 a = 10
 b = 5
+
+# NOT operator
 print(not False)
 print(not (a > b))  # a > b is True, but NOT makes it False
 
-# AND
+# AND operator
 val1 = True
 val2 = True
-print("AND operator:", val1 and val2)
+print("AND operator:", val1 and val2)  # True only if both are True
 
-# OR
+# OR operator
 val1 = True
 val2 = False
-print("OR operator:", val1 or val2)
+print("OR operator:", val1 or val2)  # True if at least one is True
