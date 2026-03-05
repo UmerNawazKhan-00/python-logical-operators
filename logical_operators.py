d92bca058dec4b0e3e42bb90e864b58735e50504
Umer Nawaz Khan <omer034000@gmail.com>
2026-03-05 22:28:14 +0500

    Added logical operators example in Python

---
+++ logical_operators.py
@@ -0,0 +1,17 @@
+# Logical Operators (AND, OR, NOT)
+
+# NOT
+a = 10
+b = 5
+print(not False)
+print(not (a > b))  # a > b is True, but NOT makes it False
+
+# AND
+val1 = True
+val2 = True
+print("AND operator:", val1 and val2)
+
+# OR
+val1 = True
+val2 = False
+print("OR operator:", val1 or val2)
